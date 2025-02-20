5621776b152b91413cdadf193eb2ce6dbf4ad88a
Dmitry Romashov <framebassman@gmail.com>
2025-02-20 01:39:57 +0100

    Try

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,11 @@ import pytest
 
 
 @pytest.fixture(scope="session")
-def logger() -> Logger:
+def logger(pytestconfig, worker_id) -> Logger:
+    if worker_id is not None:
+        logging.basicConfig(
+            format=pytestconfig.getini("log_file_format"),
+            filename=f"tests_{worker_id}.log",
+            level=pytestconfig.getini("log_file_level"),
+        )
     return logging.getLogger()
